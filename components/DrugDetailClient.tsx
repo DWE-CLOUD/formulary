aa7038d3b043ea20c05d49efac6bee9beddc01a3
DWOSCLOUD <85799123+DWE-CLOUD@users.noreply.github.com>
2025-09-02 08:48:48 +0530

    Fix coverageBreakdown object syntax

--- components/DrugDetailClient.tsx
+++ components/DrugDetailClient.tsx
@@ -135,6 +135,7 @@ export default function DrugDetailClient({ drug }: DrugDetailClientProps) {
     medicaid: 92,
     va: 100
   };
+  };
 
   return (
     <div className="min-h-screen bg-gray-50">
